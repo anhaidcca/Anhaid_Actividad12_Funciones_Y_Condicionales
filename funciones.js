06918d8100f135f3f3959aeef7117b9e7f54f4f2
anhaidcca <anhaidcca@gmail.com>
2020-12-16 16:59:38 -0600

    16dic

---
+++ funciones.js
@@ -0,0 +1,10 @@
+function comparar(){
+ const a = Number(prompt("Ingresa un número:"));
+ const b = Number(prompt ("Ingresa otro número:"));
+
+ if (a > b){
+    alert (a + " es mayor que " + b);
+ } else {
+    alert (b + " es mayor que " + a);
+ }
+}
